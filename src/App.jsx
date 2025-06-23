00916fb256ec7278d36f899259b87abedaf1ab5f
Victor Jakxel Islas Carreon <jakxel20@gmail.com>
2025-06-23 14:02:29 -0500

    new project portafolio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './components/navbar'
+
+
+function App(){
+return (
+  <div className='screen'>
+  <p>Hola como estan</p>
+  <Navbar />
+  </div>
+)
+}
+
+export default App
---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function navbar() {
+  return (
+    <div className="navbar">
+    <p>Hola babys esto es un navbar</p>
+    </div>
+  )
+}
+
+export default navbar;
